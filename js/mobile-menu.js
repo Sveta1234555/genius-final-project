3c787a68bfc2058048d80c5c1be9676850a67653
Sveta1234555 <74295497+Sveta1234555@users.noreply.github.com>
2025-04-11 15:34:26 +0300

    revision of of details - internal links, colors, centering

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,27 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 const disableScroll = () =>
-  document.body.classList.toggle('is-scroll-disabled');
+  document.body.classList.toggle("is-scroll-disabled");
 
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+//закрыть мобменю (после клика по меню-ссылке)
+const closeMenu = () => {
+  mobileMenu.classList.remove("is-open");
+  disableScroll();
+};
 
-menuBtnOpen.addEventListener('click', disableScroll);
-menuBtnClose.addEventListener('click', disableScroll);
+menuBtnOpen.addEventListener("click", toggleMenu);
+menuBtnOpen.addEventListener("click", disableScroll);
+menuBtnClose.addEventListener("click", toggleMenu);
+menuBtnClose.addEventListener("click", disableScroll);
+
+// слушать нажатия на меню, если ссылка (а)- закрыть моб меню
+// Делегирование событий:
+mobileMenu.addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
+    // Проверяем, что клик был по ссылке
+    closeMenu();
+  }
+});
--- js/modal.js
+++ js/modal.js
@@ -11,13 +11,3 @@ modalBtnOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
 modalSortOpen.addEventListener("click", toggleModal);
 modalSortClose.addEventListener("click", toggleModal);
-
-// // // const modalSort = document.querySelector(".backdrop");
-// // // // const modal = document.querySelector('.modal');
-// // const modalSortOpen = document.querySelector(".modal-sort-open");
-// // const modalSortClose = document.querySelector(".modal-sort-close");
-
-// // const toggleModal2 = () => modalSort.classList.toggle("is-hidden");
-
-// modalSortOpen.addEventListener("click", toggleModal);
-// modalSortClose.addEventListener("click", toggleModal);
